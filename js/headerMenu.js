8ae1e40ee9278e186c429412a097cd501158e9e8
Ryan Todd <r2neatha678@gmail.com>
2025-05-01 16:51:13 -0700

    Home page update and bug fixes

---
+++ js/headerMenu.js
@@ -0,0 +1,10 @@
+// js/headerMenu.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navList = document.getElementById('nav-list');
+  
+    menuToggle.addEventListener('click', () => {
+      navList.classList.toggle('active');
+    });
+  });
+  
